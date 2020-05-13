35db3a4f10fb0839663c49b6a49237786e39ddc9
Tobi Alagbe <alagbet100@gmail.com>
2020-05-13 12:00:18 +0100

    demo

--- demo.js
+++ demo.js
@@ -2,9 +2,9 @@ document.addEventListener("DOMContentLoaded", function(){
     // Handler when the DOM is fully loaded
     
     let v = new Visible(".targetElement",function() {
-        console.log("e work o");   
+        console.log("It's visible!");   
     },function() {
-        console.log("e don comot");   
+        console.log("naah..it's gone");   
     });
 
     v.watch();
